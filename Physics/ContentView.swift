e43b2f726f3c08e5042d699677d5dff9878d082d
Henry Hudson <henryhudson1431@yahoo.com>
2020-07-08 02:40:15 +0100

    Initial Commit

---
+++ Physics/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Physics
+//
+//  Created by Henry Hudson on 08/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Physics/PhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsApp.swift
+//  Physics
+//
+//  Created by Henry Hudson on 08/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
